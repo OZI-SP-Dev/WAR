ec9f544c98250cd10b2eef3ba8cbf1e633ddaed3
CLARK <1462767877.E@mil>
2020-02-25 14:39:28 -0500

    Added directory structure

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import './AppHeader.css';
-import { UserContext } from './UserProvider';
+import { UserContext } from '../../UserProvider';
 
 function AppHeader() {
   const user = useContext(UserContext);
