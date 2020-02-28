506aec1e5a4b1e94da51feaa33f6502eaab0f415
CLARK <1462767877.E@mil>
2020-02-28 14:00:57 -0500

    better web context config

--- src/providers/SPWebContext.js
+++ src/providers/SPWebContext.js
@@ -1,5 +1,7 @@
+import "@pnp/sp/sites";
 import { Web } from '@pnp/sp/webs';
+import { sp } from "@pnp/sp";
 
-export const spWebContext = new Web('https://usaf.dps.mil/teams/10251').configure({
+export const spWebContext = new Web(sp.site.getWebUrlFromPageUrl(window.location)).configure({
     headers: { "Accept": "application/json; odata=verbose" }
   });
