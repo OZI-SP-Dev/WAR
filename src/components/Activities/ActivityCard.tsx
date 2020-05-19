f84c22598c9e1c399d368d63765701f4890de865
Rob P <frogybert@users.noreply.github.com>
2020-05-19 14:26:31 -0400

    fixing jeremy's huge mistake

--- src/components/Activities/ActivityCard.tsx
+++ src/components/Activities/ActivityCard.tsx
@@ -18,7 +18,7 @@ export const ActivityCard: React.FunctionComponent<IActivityCardProps> = ({ acti
                 <Card.Title>Activity/Purpose: <span>{activity.Title}</span></Card.Title>
                 <Card.Text as="div">
                     <strong>Week of:</strong> {moment(activity.WeekOf).format("MM/DD/YYYY")}<br />
-                    <strong>Action Taken/In Work</strong> <span style={{ whiteSpace: 'pre-line' }}>{activity.ActionTaken}</span><br />
+                    <strong>Action Taken/In Work:</strong> <span style={{ whiteSpace: 'pre-line' }}>{activity.ActionTaken}</span><br />
 										<strong>Branch: </strong><span>{activity.Branch}</span><br />
 										<strong>OPRs:</strong> {activity.OPRs && activity.OPRs.results && activity.OPRs.results.map((OPR: any, index: number, array: any[]) =>
 											(<span key={OPR.Id}> {OPR.Title}{array.length-1 > index ? ';' : ''} </span>))}
