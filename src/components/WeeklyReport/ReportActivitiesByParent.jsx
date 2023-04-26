6f20879147dbb5431352e2b54220d4b98260c5da
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2023-04-26 13:43:22 -0400

    Prettier ReportActivities

--- src/components/WeeklyReport/ReportActivitiesByParent.jsx
+++ src/components/WeeklyReport/ReportActivitiesByParent.jsx
@@ -1,35 +1,50 @@
-import React, { Component } from 'react';
-import { Row } from 'react-bootstrap';
-import ReportActivity from './ReportActivity';
-import './ReportActivity.css';
+import React, { Component } from "react";
+import { Row } from "react-bootstrap";
+import ReportActivity from "./ReportActivity";
+import "./ReportActivity.css";
 
 class ReportActivitiesByParent extends Component {
+  getActivitiesByParent() {
+    let activitiesByBranch = {};
+    this.props.activities.forEach((activity) => {
+      let branch = activity.Branch.substr(0, 3);
+      if (!activitiesByBranch[branch]) {
+        activitiesByBranch[branch] = [];
+      }
+      activitiesByBranch[branch].push(activity);
+    });
+    return activitiesByBranch;
+  }
 
-    getActivitiesByParent() {
-        let activitiesByBranch = {};
-        this.props.activities.forEach(activity => {
-            let branch = activity.Branch.substr(0, 3);
-            if (!activitiesByBranch[branch]) {
-                activitiesByBranch[branch] = []
-            }
-            activitiesByBranch[branch].push(activity);
-        });
-        return activitiesByBranch;
-    }
+  render() {
+    let activitiesByBranch = this.getActivitiesByParent();
 
-    render() {
-
-        let activitiesByBranch = this.getActivitiesByParent();
-
-        return (
-            this.props.activities.length > 0 ? Object.keys(activitiesByBranch).map(branch =>
-                (<div className="activity-branch">
-                    <Row><h4><u>{branch} Activities</u>:</h4></Row>
-                    {activitiesByBranch[branch].map(activity => <ReportActivity activity={activity} hideWeekOf={this.props.hideWeekOf} />)}
-                </div>)
-            ) : <Row className="justify-content-center m-5"><h4>{this.props.reportGenerated ? "No activities were found for the given date range" : "Please select a date range and press 'Generate Report' for a report to be shown"}</h4></Row>
-        );
-    }
+    return this.props.activities.length > 0 ? (
+      Object.keys(activitiesByBranch).map((branch) => (
+        <div className="activity-branch">
+          <Row>
+            <h4>
+              <u>{branch} Activities</u>:
+            </h4>
+          </Row>
+          {activitiesByBranch[branch].map((activity) => (
+            <ReportActivity
+              activity={activity}
+              hideWeekOf={this.props.hideWeekOf}
+            />
+          ))}
+        </div>
+      ))
+    ) : (
+      <Row className="justify-content-center m-5">
+        <h4>
+          {this.props.reportGenerated
+            ? "No activities were found for the given date range"
+            : "Please select a date range and press 'Generate Report' for a report to be shown"}
+        </h4>
+      </Row>
+    );
+  }
 }
 
 export default ReportActivitiesByParent;
--- src/components/WeeklyReport/ReportActivity.jsx
+++ src/components/WeeklyReport/ReportActivity.jsx
@@ -1,24 +1,41 @@
-import React, { Component } from 'react';
-import { Row } from 'react-bootstrap';
-import DateUtilities from '../../utilities/DateUtilities';
-import './ReportActivity.css';
+import React, { Component } from "react";
+import { Row } from "react-bootstrap";
+import DateUtilities from "../../utilities/DateUtilities";
+import "./ReportActivity.css";
 
 class ReportActivity extends Component {
-
-    render() {
-        let activity = this.props.activity;
-        return (
-            <Row className="activity">
-                <p className="preserve-whitespace">
-                    {this.props.hideWeekOf ? '' : <><strong>Week of:</strong> {DateUtilities.getDate(activity.WeekOf).format("MM/DD/YYYY")} <br /></>}
-                    <strong>Activity/Purpose:</strong> {activity.Title}<br />
-                    <strong>Action Taken/In Work:</strong> {activity.ActionTaken}<br />
-                    <strong>OPRs:</strong> {activity.OPRs && activity.OPRs.results && activity.OPRs.results.map((OPR, index, array) =>
-                        (<span key={OPR.Id}> {OPR.Title}{array.length - 1 > index ? ';' : ''} </span>))}
-                </p>
-            </Row>
-        );
-    }
+  render() {
+    let activity = this.props.activity;
+    return (
+      <Row className="activity">
+        <p className="preserve-whitespace">
+          {this.props.hideWeekOf ? (
+            ""
+          ) : (
+            <>
+              <strong>Week of:</strong>{" "}
+              {DateUtilities.getDate(activity.WeekOf).format("MM/DD/YYYY")}{" "}
+              <br />
+            </>
+          )}
+          <strong>Activity/Purpose:</strong> {activity.Title}
+          <br />
+          <strong>Action Taken/In Work:</strong> {activity.ActionTaken}
+          <br />
+          <strong>OPRs:</strong>{" "}
+          {activity.OPRs &&
+            activity.OPRs.results &&
+            activity.OPRs.results.map((OPR, index, array) => (
+              <span key={OPR.Id}>
+                {" "}
+                {OPR.Title}
+                {array.length - 1 > index ? ";" : ""}{" "}
+              </span>
+            ))}
+        </p>
+      </Row>
+    );
+  }
 }
 
 export default ReportActivity;
